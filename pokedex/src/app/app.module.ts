c0f066b1452e7cde9a068bbc93874a499f020478
Evelin Ferraz <evelin.moitinho@capgemini.com>
2023-01-20 18:38:21 -0300

    [07] - Implementando primeira requisição no service

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 //Modules
 import { PagesModule } from './pages/pages.module';
-import { SharedModule } from './shared/shared.module';
 
 //Components
 
@@ -17,7 +16,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
 
-  constructor() { }
+  private url: string = 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=100';
+  constructor(private http: HttpClient) { }
+
+  get listAllPokemons(): Observable<any> {
+    return this.http.get<any>(this.url).pipe(
+      tap(res => res),
+      tap( res => {
+        console.log(res);
+      })
+    )
+  }
 }
